7420c3caa86316e27b0b7047d029ba54af0a6abb
carolinee1234 <54036574+carolinee1234@users.noreply.github.com>
2021-04-26 16:44:30 -0300

    ajustes iniciais

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAC0EMZXKXk4wI2XaqP7Y_JxsdiHDcBgBs",
+    authDomain: "primeiro-projeto-1f940.firebaseapp.com",
+    projectId: "primeiro-projeto-1f940",
+    storageBucket: "primeiro-projeto-1f940.appspot.com",
+    messagingSenderId: "479094832190",
+    appId: "1:479094832190:web:161f1d88b23e251af4656f"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAC0EMZXKXk4wI2XaqP7Y_JxsdiHDcBgBs",
+    authDomain: "primeiro-projeto-1f940.firebaseapp.com",
+    projectId: "primeiro-projeto-1f940",
+    storageBucket: "primeiro-projeto-1f940.appspot.com",
+    messagingSenderId: "479094832190",
+    appId: "1:479094832190:web:161f1d88b23e251af4656f"
+  }
 };
 
 /*
